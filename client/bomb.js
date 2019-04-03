f5f09141e291faca2f9205ee14f7ac7a91df9043
unknown <wodn040574@gmail.com>
2019-04-03 14:39:38 +0900

    수정

--- client/bomb.js
+++ client/bomb.js
@@ -13,18 +13,4 @@ function button2_click() {
     new Array(5).fill(0),
     new Array(5).fill(0)
   );
-  for (let i = 0; i < 5; i++) {
-    for (let j = 0; j < 5; j++) {
-      let ReturnValue = 0;
-      const rand = Math.random() * 100;
-
-      const arrPercent = [45.0, 55.0];
-      arr[i][j] = Math.random() * 1;
-    }
-  }
-  for (let i = 0; i < 5; i++) {
-    for (let j = 0; j < 5; j++) {
-      console.log(arr[i][j]);
-    }
-  }
 }
