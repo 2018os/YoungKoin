3b6befac991de2acc9ae44edb5ad88945c97b574
unknown <wodn040574@gamil.com>
2019-05-07 00:07:54 +0900

    name change

--- client/bomb.js
+++ client/bomb.js
@@ -1,11 +1,5 @@
 function button2_click() {
-  // let arr = [
-  //   [0, 0, 0, 0, 0],
-  //   [0, 0, 0, 0, 0],
-  //   [0, 0, 0, 0, 0],
-  //   [0, 0, 0, 0, 0],
-  //   [0, 0, 0, 0, 0]
-  // ];
+  
   let arr = new Array(
     new Array(5).fill(0),
     new Array(5).fill(0),
