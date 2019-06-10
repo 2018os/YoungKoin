0a043dec67db58d3f7735e7df0bbbe6cf295e5a7
Jae_U <wodn040574@gmail.com>
2019-06-10 23:45:34 +0900

    add ladder.js

--- client/js/ladder.js
+++ client/js/ladder.js
@@ -3,12 +3,21 @@ function laddergo() {
   let hol = false;
   let jjak = false;
   let countArr = new Array();
+  let userC;
 
   ladderC = Math.floor(Math.random() * 10 + 1);
 
   for (let i = 0; i < ladderC; i++) {
     count = Math.floor(Math.random() * 2);
     countArr[i] = count;
+  }
+
+  //choice right
+  if (userC == "right") {
+  }
+
+  //choice left
+  if (userC == "left") {
   }
 
   console.log(countArr);
