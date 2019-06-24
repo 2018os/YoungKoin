cbc5fec5f18c7914b6c23f88d5744ff3d9c45e73
2018os <tmdduq4536@naver.com>
2019-06-24 10:01:25 +0900

    create page

---
+++ client/pages/bomb.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Bomb = () => {
+  return <div>Bomb</div>;
+};
+
+export default Bomb;
--- client/pages/holjjak.js
+++ client/pages/holjjak.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Holjjak = () => {
-  return <div>Holjjak</div>;
+  const [something, setSomething] = useState("");
+  const onClickBtn = e => {
+    e.preventDefault();
+    setSomething(e.target.value);
+  };
+  const onSubmitForm = e => {
+    e.preventDefault();
+    if (something === "HOL") {
+      console.log("Win");
+    } else {
+      console.log("Lose");
+    }
+    setSomething("");
+  };
+  return (
+    <div>
+      <form onSubmit={e => onSubmitForm(e)}>
+        <button onClick={e => onClickBtn(e)} value="HOL">
+          HOL
+        </button>
+        <button onClick={e => onClickBtn(e)} value="JJAK">
+          JJAK
+        </button>
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default Holjjak;
